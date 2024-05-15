d671f3a4d3838f74fab23077d19e232b75573576
Robert Jansen <jansenrobert4@gmail.com>
2024-05-15 22:39:02 +0200

    fixed complete server, first test was ok with packet sender on local host

--- unoReverse.c
+++ unoReverse.c
@@ -42,14 +42,17 @@ void OSCleanup( void )
 
 struct ThreadArgs {
     int client_socket;
-    FILE* log_file;
-    char client_address[INET6_ADDRSTRLEN];
+    FILE* file_ptr;
+    char client_address_str[INET6_ADDRSTRLEN];
 };
 
+
 int initServer();
-int connectClient(int server_socket, char *client_address);
-void getRequest(const char *client_address, FILE *log_file, char *thread_id_str);
-void sendData(int client_socket, FILE *log_file, char *thread_id_str);
+int init_HTTP_client();
+int connectClient(int server_socket, char *client_address_str, int client_addr_str_size);
+void getRequest(const char *client_address_str, FILE *log_file, char *thread_id_str);
+void sendData(int client_socket, FILE *file_ptr, char *client_address_str);
+void cleanup(int client_socket);
 void* threadExecution(void* arg);
 
 int main() {
@@ -64,137 +67,343 @@ int main() {
 	 
     int server_socket = initServer();
 
-  while (1) {
-    struct sockaddr_storage client_addr;
-    socklen_t addr_size = sizeof client_addr;
-    int client_socket = accept(server_socket, (struct sockaddr*)&client_addr, &addr_size);
-    if (client_socket == -1) {
-        perror("accept");
-        close(server_socket);
-        exit(EXIT_FAILURE);
-    }
+	char client_address_str[INET6_ADDRSTRLEN];
+	while (1) {  
 
-    char client_address[INET6_ADDRSTRLEN];
+    int client_socket = connectClient(server_socket, client_address_str, sizeof(client_address_str));	
 	
-	
-    inet_ntop(client_addr.ss_family, &((struct sockaddr_in*)&client_addr)->sin_addr, client_address, INET6_ADDRSTRLEN);
+     //  nog niet nodig : inet_ntop(client_addr.ss_family, &((struct sockaddr_in*)&client_addr)->sin_addr, client_address_str, INET6_ADDRSTRLEN);
 
     pthread_t thread;
+	
     struct ThreadArgs* args = (struct ThreadArgs*)malloc(sizeof(struct ThreadArgs));
     args->client_socket = client_socket;
-    args->log_file = log_file;
-    strncpy(args->client_address, client_address, INET6_ADDRSTRLEN - 1); // Copy client address to args struct
-    args->client_address[INET6_ADDRSTRLEN - 1] = '\0'; // Ensure null-termination
+    args->file_ptr = file_ptr;
+    strncpy(args->client_address_str, client_address_str, INET6_ADDRSTRLEN - 1); // Copy client addinternet_address_results to args struct
+	
+    args->client_address_str[INET6_ADDRSTRLEN - 1] = '\0'; // Ensure null-termination
 
-    pthread_create(&thread, NULL, threadExecution, args);
-}
+    int thread_create_result = pthread_create(&thread, NULL, threadExecution, args);  //create a thread for each client
+		if(thread_create_result!=0)
+		{	
+		 fprintf(stderr, "Failed to create thread: %d\n", thread_create_result);
+		}
+		
+	}
 
-    fclose(log_file);
+    fclose(file_ptr);
+	
+	cleanup(server_socket);
+	
+	OSCleanup();
+	
     return 0;
 }
 
+
 int initServer() {
-    struct addrinfo internet_adress_setup, 
-	struct addrinfo *internet_adress_result;
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE;
+   struct addrinfo internet_address_setup;
+    struct addrinfo * internet_address_result;
+    memset( &internet_address_setup, 0, sizeof internet_address_setup );
+    internet_address_setup.ai_family = AF_UNSPEC;
+    internet_address_setup.ai_socktype = SOCK_STREAM;
+    internet_address_setup.ai_flags = AI_PASSIVE;
 
-    int status = getaddrinfo(NULL, SERVERPORT, &hints, &res);
-    if (status != 0) {
-        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+    int getaddrinfo_return = getaddrinfo(NULL, SERVERPORT, &internet_address_setup, &internet_address_result); // server is listening on port 22
+    if (getaddrinfo_return != 0) 
+	{
+        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(getaddrinfo_return));
         exit(EXIT_FAILURE);
     }
+ int internet_socket = -1;
+        struct addrinfo * internet_address_result_iterator = internet_address_result;
+        while( internet_address_result_iterator != NULL )
+        {
+            //Step 1.2
+            internet_socket = socket( internet_address_result_iterator->ai_family, internet_address_result_iterator->ai_socktype, internet_address_result_iterator->ai_protocol );
+            int mode = 0;
+            setsockopt(internet_socket, IPPROTO_IPV6, IPV6_V6ONLY, (char*)&mode, sizeof(mode));
+            if( internet_socket == -1 )
+            {
+                perror( "socket" );
+            }
+            else
+            {
+                //Step 1.3
+                int bind_return = bind( internet_socket, internet_address_result_iterator->ai_addr, internet_address_result_iterator->ai_addrlen );
+                if( bind_return == -1 )
+                {
+                    perror( "bind" );
+                    close( internet_socket );
+                }
+                else
+                {
+                    //Step 1.4
+                    int listen_return = listen( internet_socket, 1 );
+                    if( listen_return == -1 )
+                    {
+                        close( internet_socket );
+                        perror( "listen" );
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            internet_address_result_iterator = internet_address_result_iterator->ai_next;
+        }
 
-    int server_socket = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-    if (server_socket == -1) {
-        perror("socket");
-        exit(EXIT_FAILURE);
-    }
+        freeaddrinfo( internet_address_result );
 
-    if (bind(server_socket, res->ai_addr, res->ai_addrlen) == -1) {
-        perror("bind");
-        close(server_socket);
-        exit(EXIT_FAILURE);
-    }
+        if( internet_socket == -1 )
+        {
+            fprintf( stderr, "socket: no valid socket address found\n" );
+            exit( 2 );
+        }
 
-    if (listen(server_socket, SOMAXCONN) == -1) {
-        perror("listen");
-        close(server_socket);
-        exit(EXIT_FAILURE);
-    }
-
-    freeaddrinfo(res);
-    return server_socket;
+        return internet_socket	;	
 }
 
-int connectClient(int server_socket, char *client_address) {
+int init_HTTP_client()
+{
+	struct addrinfo internet_address_setup;
+    struct addrinfo * internet_address_result;
+    memset( &internet_address_setup, 0, sizeof internet_address_setup );
+    internet_address_setup.ai_family = AF_UNSPEC;
+    internet_address_setup.ai_socktype = SOCK_STREAM;
+    internet_address_setup.ai_flags = AI_PASSIVE;
+	
+        int getaddrinfo_return = getaddrinfo( "ip-api.com", "80", &internet_address_setup, &internet_address_result );
+        if( getaddrinfo_return != 0 )
+        {
+            fprintf( stderr, "getaddrinfo: %s\n", gai_strerror( getaddrinfo_return ) );
+            exit( 1 );
+        }
+
+        int internet_socket = -1;
+        struct addrinfo * internet_address_result_iterator = internet_address_result;
+        while( internet_address_result_iterator != NULL )
+        {
+            internet_socket = socket( internet_address_result_iterator->ai_family, internet_address_result_iterator->ai_socktype, internet_address_result_iterator->ai_protocol );
+            if( internet_socket == -1 )
+            {
+                perror( "socket" );
+            }
+            else
+            {
+                int connect_return = connect( internet_socket, internet_address_result_iterator->ai_addr, internet_address_result_iterator->ai_addrlen );
+                if( connect_return == -1 )
+                {
+                    perror( "connect" );
+                    close( internet_socket );
+                }
+                else
+                {
+                    break;
+                }
+            }
+            internet_address_result_iterator = internet_address_result_iterator->ai_next;
+        }
+
+        freeaddrinfo( internet_address_result );
+
+        if( internet_socket == -1 )
+        {
+            fprintf( stderr, "socket: no valid socket address found\n" );
+            exit( 2 );
+        }
+
+        return internet_socket;
+}
+
+
+
+int connectClient(int server_socket, char *client_address_str, int client_address_str_lenght ) 
+{
     struct sockaddr_storage client_addr;
-    socklen_t addr_size = sizeof client_addr;
-    int client_socket = accept(server_socket, (struct sockaddr*)&client_addr, &addr_size);
+    socklen_t client_addr_size = sizeof client_addr;
+    int client_socket = accept(server_socket, (struct sockaddr*)&client_addr, &client_addr_size);
     if (client_socket == -1) {
         perror("accept");
         close(server_socket);
         exit(EXIT_FAILURE);
     }
+		//get IP address from client
+        if (client_addr.ss_family == AF_INET) //IPv4
+		{ 		
+			struct sockaddr_in* s = (struct sockaddr_in*)&client_addr;
+            inet_ntop(AF_INET, &s->sin_addr, client_address_str, client_address_str_lenght);
+		} else { // AF_INET6
+            // IPv6 address
+            struct sockaddr_in6* s = (struct sockaddr_in6*)&client_addr;
+            inet_ntop(AF_INET6, &s->sin6_addr, client_address_str, client_address_str_lenght);
+        }
 
-    inet_ntop(client_addr.ss_family, &((struct sockaddr_in*)&client_addr)->sin_addr, client_address, INET6_ADDRSTRLEN);
+        printf("Client IP address: %s\n", client_address_str);
+	
     return client_socket;
 }
 
-void getRequest(const char *client_address, FILE *log_file, char *thread_id_str) {
-    int api_socket = initServer();
-    FILE *api_file = fdopen(api_socket, "r+");
 
-    char request[1024];
-    sprintf(request, "GET /json/%s HTTP/1.0\r\nHost: ip-api.com\r\nConnection: close\r\n\r\n", client_address);
-    fprintf(api_file, "%s", request);
-    fflush(api_file);
 
-    char response[1024];
-    fgets(response, sizeof(response), api_file);
+void getRequest(const char *client_address_str, FILE *file_ptr, char *thread_id_str) {
+    int API_socket_HTTP  = init_HTTP_client();
+   
+    //print IP address in log file
+    fputs("Thread ID: ", file_ptr);
+    fputs(thread_id_str, file_ptr);
+    fputs(" The IP address =",file_ptr);
+    fputs(client_address_str,file_ptr);
+    fputs("\n",file_ptr);
 
-    char* json_start = strchr(response, '{');
-    if (json_start == NULL) {
-        perror("strchr");
-        fclose(api_file);
-        close(api_socket);
-        return;
+    char buffer[1000];
+    char HTTPrequest[100] ={0};
+
+
+    sprintf(HTTPrequest, "GET /json/%s HTTP/1.0\r\nHost: ip-api.com\r\nConnection: close\r\n\r\n", client_address_str);
+    printf("HTTP request = %s",HTTPrequest);
+
+    //SEND HTTPREQUEST TO GET LOCATION OF BOTNET
+    int number_of_bytes_send = 0;
+    number_of_bytes_send = send( API_socket_HTTP, HTTPrequest , strlen(HTTPrequest), 0 );
+    if( number_of_bytes_send == -1 )
+    {
+        perror( "send" );
     }
 
-    fprintf(log_file, "Thread ID: %s, Geolocation: %s\n", thread_id_str, json_start);
-    fclose(api_file);
-    close(api_socket);
-}
+    //receive json data and cut the HTTP header off
+    int number_of_bytes_received = 0;
+    number_of_bytes_received = recv( API_socket_HTTP, buffer, ( sizeof buffer ) - 1, 0 );
+    if( number_of_bytes_received == -1 )
+    {
+        perror( "recv" );
+    }
+    else
+    {
+        buffer[number_of_bytes_received] = '\0';
+        printf( "Received : %s\n", buffer );
+    }
+	
+ char* jsonFile = strchr(buffer,'{');
 
-void sendData(int client_socket, FILE *log_file, char *thread_id_str) {
-    char buffer[1024];
-    int total_bytes_sent = 0;
-    while (1) {
-        int bytes_sent = send(client_socket, "I like beer!!!\n", 17, 0);
-        if (bytes_sent == -1) {
-            perror("send");
-            break;
-        } else if (bytes_sent == 0) {
-            break;
+    if( jsonFile == NULL){
+        number_of_bytes_received = recv( API_socket_HTTP, buffer, ( sizeof buffer ) - 1, 0 );
+        if( number_of_bytes_received == -1 )
+        {
+            perror( "recv" );
         }
-        total_bytes_sent += bytes_sent;
-        usleep(100000);
+        else
+        {
+            buffer[number_of_bytes_received] = '\0';
+            printf("Received : %s\n", buffer );
+        }
+
+        //put geolocation in file
+        fputs("Thread ID: ", file_ptr);
+        fputs(thread_id_str, file_ptr);
+        fputs("\tGeolocation = ",file_ptr);
+        fputs( buffer , file_ptr );
+        fputs("\n",file_ptr);
+    } else{
+
+        //put geolocation in file
+        fputs("Thread ID: ", file_ptr);
+        fputs(thread_id_str, file_ptr);
+        fputs("\tGeolocation = ",file_ptr);
+        fputs(jsonFile, file_ptr);
+        fputs("\n",file_ptr);
     }
-    fprintf(log_file, "Thread ID: %s, Total bytes sent: %d\n", thread_id_str, total_bytes_sent);
+
+    //clean HTTP socket
+    cleanup(API_socket_HTTP);
 }
 
-void* threadExecution(void* arg) {
+void sendData(int client_socket, FILE *file_ptr, char *client_address_str){	
+
+
+    pthread_t thread_id = pthread_self();
+    char thread_id_str[20];
+    sprintf(thread_id_str, "%ld", thread_id);
+
+    int number_of_bytes_received = 0;
+    char buffer[100];
+
+    number_of_bytes_received = recv(client_socket, buffer, (sizeof buffer) - 1, 0);
+    if (number_of_bytes_received == -1) {
+        perror("recv");
+    } else {
+        buffer[number_of_bytes_received] = '\0';
+        printf("Received : %s\n", buffer);
+    }
+    // get the geolocation
+    getRequest(client_address_str, file_ptr,thread_id_str);
+
+    //put the message that the client sent in the log file
+    fputs("Thread ID: ", file_ptr);
+    fputs(thread_id_str, file_ptr);
+    fputs(" Client sent = ",file_ptr);
+    fputs(buffer, file_ptr);
+    fputs("\n",file_ptr);
+    //Step 3.1
+    int number_of_bytes_send = 0;
+    int totalBytesSend = 0;
+
+    char totalBytesSendStr[20];
+
+
+    while(1){
+
+        //UNO REVERSE TIME
+        number_of_bytes_send = send( client_socket, "Never gonna give you up\nNever gonna let you down\nNever gonna run around and desert you\n"
+		"Never gonna make you cry\nNever gonna say goodbye\nNever gonna tell a lie and hurt you\nNever gonna give you up\nNever gonna let you down\n"
+		"Never gonna run around and desert you\nNever gonna make you cry\nNever gonna say goodbye\nNever gonna tell a lie and hurt you\n"
+		"Never gonna give you up\nNever gonna let you down\nNever gonna run around and desert you\nNever gonna make you cry\n"
+		"Never gonna say goodbye\nNever gonna tell a lie and hurt you\n", 534, 0);
+
+        //check if client left and put number of bytes send in the log file
+        if( number_of_bytes_send == -1 )
+        {
+            printf("Client left. I sent %d bytes\n",totalBytesSend);
+            sprintf(totalBytesSendStr, "%d", totalBytesSend);
+            fputs("Thread ID: ", file_ptr);
+            fputs(thread_id_str, file_ptr);
+            fputs(" Total bytes send = ",file_ptr);
+            fputs(totalBytesSendStr, file_ptr);
+            fputs("\n",file_ptr);
+            break;
+        }else{
+            totalBytesSend +=number_of_bytes_send;
+            usleep(100000);// Just here to test or i wil attack myself to hard
+        }
+    }
+    cleanup(client_socket);
+
+}
+
+void* threadExecution(void* arg) 
+{
     struct ThreadArgs* args = (struct ThreadArgs*)arg;
     char thread_id_str[20];
     pthread_t thread_id = pthread_self();
     sprintf(thread_id_str, "%ld", thread_id);
 
-    getRequest(args->client_address, args->log_file, thread_id_str);
-    sendData(args->client_socket, args->log_file, thread_id_str);
+    getRequest(args->client_address_str, args->file_ptr, thread_id_str);
+    sendData(args->client_socket, args->file_ptr, thread_id_str);
 
     close(args->client_socket);
     free(args);
     pthread_exit(NULL);
 }
+
+void cleanup( int client_socket)
+{
+	int shutdown_return = shutdown( client_socket, SD_RECEIVE );
+	if( shutdown_return == -1 )
+	{
+		perror( "shutdown" );
+	}
+
+	close( client_socket );
+}
+
